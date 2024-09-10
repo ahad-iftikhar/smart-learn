80ff867a5341d6ac07764477c8dee89321c9ae7b
Zunair Ali <154310361+Zunair890@users.noreply.github.com>
2024-09-10 11:08:16 +0500

    Updated navbar

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -81,8 +81,8 @@ const HomePage = () => {
                 <Image
                   src="/assets/dashboard.jpg"
                   alt="banner image"
-                  width={1000}
-                  height={1000}
+                  width={1050}
+                  height={1050}
                   quality={100}
                   className="rounded-md lg:rounded-xl bg-foreground/10 shadow-2xl ring-1 ring-border"
                 />
